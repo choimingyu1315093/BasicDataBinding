b9d2d40157d617da1be4b59debd7676b169d0d56
choimingyu <alsrb1315093@gmail.com>
2024-11-18 08:51:36 +0900

    SimpleDataBinding 완료

--- app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
+++ app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
@@ -8,25 +8,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.anushka.bindingdemo1.databinding.ActivityMainBinding
+import com.anushka.bindingdemo1.model.Student
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val button = findViewById<Button>(R.id.submit_button)
-        button.setOnClickListener {
-            displayGreeting()
-        }
-    }
-    private fun displayGreeting() {
-        val messageView = findViewById<TextView>(R.id.greeting_text_view)
-        val nameText = findViewById<EditText>(R.id.name_edit_text)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.student = getStudent()
 
-        messageView.text = "Hello! "+ nameText.text
+//        val student = getStudent()
+//        binding.tvName.text = student.name
+//        binding.tvEmail.text = student.email
+    }
+
+    private fun getStudent(): Student {
+        return Student(1, "Alex", "john.mckinley@examplepetstore.com")
     }
 }
---
+++ app/src/main/java/com/anushka/bindingdemo1/model/Student.kt
@@ -0,0 +1,7 @@
+package com.anushka.bindingdemo1.model
+
+data class Student(
+    var id: Int,
+    var name : String,
+    var email: String
+)
