4656068790e55679e18d690e291323fca21864f2
choimingyu <alsrb1315093@gmail.com>
2024-11-25 13:35:42 +0900

    ViewBinding 추가

---
+++ app/src/main/java/com/anushka/bindingdemo1/SecondActivity.kt
@@ -0,0 +1,20 @@
+package com.anushka.bindingdemo1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.anushka.bindingdemo1.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
